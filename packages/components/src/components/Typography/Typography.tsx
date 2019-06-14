7e3601862ede748771621045bf8083b6e3b37d5f
Daniel Brodin <daniel@itiden.se>
2019-06-14 13:20:07 +0200

    Add Text component

---
+++ packages/components/src/components/Typography/Typography.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Title = styled.h2`
+  color: #4f4f4f;
+  font-family: 'Roboto', Arial, Helvetica, sans-serif;
+  font-size: 26px;
+  margin: 0;
+  text-transform: uppercase;
+`;
+
+export const Text = styled.span`
+  color: #333;
+  font-family: 'Roboto', Arial, Helvetica, sans-serif;
+`;
